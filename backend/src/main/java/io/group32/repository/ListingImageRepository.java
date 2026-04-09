71b37ed1426d0f297e2738257ac32c2efa1e77b1
lloydoshea <ohlloydy874@gmail.com>
2026-04-09 14:16:18 +0100

    Update ListingImageRepository.java
    NEEDED

--- backend/src/main/java/io/group32/repository/ListingImageRepository.java
+++ backend/src/main/java/io/group32/repository/ListingImageRepository.java
@@ -5,7 +5,9 @@ import io.group32.model.ListingImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ListingImageRepository extends JpaRepository<ListingImage, Long> {
     List<ListingImage> findByListing(Listing listing);
+    Optional<ListingImage> findByPublicId(String publicId);
 }
